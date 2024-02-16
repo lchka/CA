509c6a63caecd434fb6063ceab53118095dfb8a6
Laura Hofmanova <N00222003@iadt.ie>
2024-02-16 14:37:52 +0000

    only drawing one barchart at a time

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+// import { StackedBarchart } from './stackedBarchart.js';
 let barCharts = [];
 let data;
 let cleanData = [];
@@ -83,9 +84,68 @@ function setup() {
     yValue: "cell-usage",
     xValue: "Year",
   };
- 
-  //push new barchart
-  barCharts.push(new BarChart(barChart01));
+
+  let barChart02 = {
+    data: cleanData,
+    chartHeight: 200,
+    chartWidth: 350,
+    xPos: 800,
+    yPos: 570,
+    barWidth: 25,
+    genFont:font,
+    fontBold:boldText,
+
+    //colours
+    barFill: ["#F7BDD1", "#F59EBB", "#F17CA3"],
+    axisLineColour: "#d9d9d9",
+    ticksColour: "#C72A2A",
+
+    //text for X AXIS
+    textColour: "#0f0000",
+    textRotate: 45,
+    textSizeText: 13 ,
+
+    //text for col y axis name
+
+    colYAxisColour:"#C72A2A",
+    colYAxisSize:16,
+    colYAxisRotation:-90,
+    colYAxisStyle:BOLD,
+    colYAxisTextValue:"no. of deaths",
+    colYAxisTextX:50,
+    colYAxisTextY:-40,
+
+    //text for col
+    textSizeColText: 16,
+    colLabel: "accidents per year",
+    textColY: 60,
+    textColX: 180,
+    textColWeight: BOLD,
+    colVertAlign:CENTER,
+    colHorzAlign:CENTER,
+
+    //text for title
+    textSizeTitle: 24,
+    titleText: "Fatal driving accidents resulting from the use of mobile devices",
+    textTitleX: -25,
+    textTitleY: 270,
+    titlePaddingX: 400,
+    titleWeight: BOLD,
+    titleHorzAlign:CENTER,
+    titleVertAlign:CENTER,
+
+    //tick and tick text
+    numTicks: 10,
+    ticksTextSize: 13,
+    tickStyle: BOLD,
+
+    //values
+    yValue: "cell-usage",
+    xValue: "Year",
+  };
+  barCharts.push(new BarChart(barChart01))
+
+  barCharts.push(new StackedBarchart(barChart02))
 
 }
 
