22c2b65496e62c2670db176174c44ac94994b22e
Laura Hofmanova <N00222003@iadt.ie>
2024-02-18 02:03:59 +0000

    fixing xpos and ypos moving all graphs

--- HorzBarChart.js
+++ HorzBarChart.js
@@ -1,6 +1,6 @@
 //fix scaling, first loop is already the max, how to set the max from the array of cleanData, with the lowest value as first tick
 
-class LineGraphChart {
+class HorzBarChart {
   constructor(obj) {
     //canvas
     this.canvasWidth = obj.canvasWidth;
@@ -13,29 +13,21 @@ class LineGraphChart {
     this.xPos = obj.xPos;
     this.yPos = obj.yPos;
     this.axisLineColour = obj.axisLineColour;
+    this.barWidth = obj.barWidth;
     this.yValue = obj.yValue;
-    this.pointEllipseSize = obj.pointEllipseSize;
-    this.chartXYLineWeight = obj.chartXYLineWeight;
 
     //ticks
     this.numTicks = obj.numTicks;
     this.ticksTextSize = obj.ticksTextSize;
     this.tickStyle = obj.tickStyle;
-    this.tickTextXPos = obj.tickTextXPos;
-    this.ticksLength = obj.ticksLength;
 
-    //TEXT x axis
+    //TEXT
     this.textSizeText = obj.textSizeText;
     this.textSizeColText = obj.textSizeColText;
     this.textRotate = obj.textRotate;
     this.xValue = obj.xValue;
     this.genFont = obj.genFont;
     this.fontBold = obj.fontBold;
-    this.indiLineRotate = this.indiLineRotate;
-    this.indiLineWeight = obj.indiLineWeight;
-    this.xAxisTextYPos = obj.xAxisTextYPos;
-    this.indiLineHeight = obj.indiLineHeight;
-
     //subtext
 
     this.subLabel = obj.subLabel; //to pull the section name from the csv file
@@ -43,8 +35,7 @@ class LineGraphChart {
     this.textSubY = obj.textSubY;
     this.subVertAlign = obj.subVertAlign;
     this.subHorzAlign = obj.subHorzAlign;
-    this.textSizeSub=obj.textSizeSub;
-
+    this.textSizeSub = obj.textSizeSub;
     //text col y axis name
 
     this.colYAxisColour = obj.colYAxisColour;
@@ -68,36 +59,39 @@ class LineGraphChart {
     //colors
     this.barFill = obj.barFill;
     this.textColour = obj.textColour;
-    this.textTitleColour=obj.textTitleColour;
     this.bColour = obj.bColour;
-    this.ticksValueColour = obj.ticksValueColour;
-    this.pointsColour = obj.pointsColour;
-    this.chartLineColour = obj.chartLineColour;
-    this.chartLineIndiLineColour = obj.chartLineIndiLineColour;
+    this.ticksColour = obj.ticksColour;
     this.subTextColour=obj.subTextColour
 
+
     // Calculate maxValue and scale
-    this.maxValue = max(this.data.map((d) => d[this.yValue])); // Get the max value from the yValue of the chart
+    this.maxValue = max(this.data.map((d) => d[this.yValue])); // Get the max height of the chart
     this.scale = this.chartHeight / this.maxValue; // Calculate the scale for the chart
   }
 
   render() {
-    //creates the graphs lines
+    // for (let i = 0; i < 1000; i++) {
+    //   if (this.scale % this.numTicks == 0) {
+    //     break;
+    //   } else {
+    //     this.scale = this.scale + 1;
+    //   }
+    // }
+
     push();
     translate(this.xPos, this.yPos);
     stroke(this.axisLineColour);
-    strokeWeight(this.chartXYLineWeight);
     line(0, 0, 0, -this.chartHeight);
     line(0, 0, this.chartWidth, 0);
 
-    //
+    // Map for labels x is just a name
     let XLabels = this.data.map((x) => x[this.xValue]);
 
     // Draw ticks on y-axis
     for (let i = 0; i <= this.numTicks; i++) {
       push();
       translate(0, i * (-this.chartHeight / this.numTicks));
-      line(0, 0, this.ticksLength, 0);
+      line(0, 0, -5, 0);
       pop();
     }
 
@@ -105,68 +99,49 @@ class LineGraphChart {
     let tickValue = this.maxValue / this.numTicks; //prevents it from going over the max of preset value in the column/rows. On first loop it did display the max value (453) but kept going over it, which is why we needed another variable that handles the 'gap difference' between each value label.
     for (let i = 0; i <= this.numTicks; i++) {
       push();
-      translate(0, (i * -this.chartHeight) / this.numTicks); //chartheight is always in - as the original 0,0 is above the plus.
+      translate(0, (i * -this.chartHeight) / this.numTicks);
       noStroke();
       textSize(this.ticksTextSize);
-      textFont(this.fontBold);
-      fill(this.ticksValueColour);
+      textStyle(this.tickStyle);
+      fill(this.ticksColour);
       textAlign(RIGHT, CENTER);
       textFont(this.genFont);
-
-      text(Math.ceil(i * tickValue), this.tickTextXPos, 0); //everytime it loops it adds from the previous loop to the current one, used math,ciel to round to the nearest whole number
+      9;
+      text(Math.ceil(i * tickValue), -10, 0); //everytime i loop it adds from the previous loop to the current one
       pop();
     }
 
-    // Drawing chart lines and points
+    // Calculate gap
+    let gap =
+      (this.chartWidth - this.data.length * this.barWidth) /
+      (this.data.length + 1);
 
-    let xStep = this.chartWidth / (this.data.length - 1); //350/11-1=35
-    let xLine = 0; //will be responsible in moving from point a to point b
-    let yStep = this.chartHeight / this.maxValue; //200/2843=0.0706
-    beginShape();
-    noFill();
-
+    // Draw bars
+    push();
+    translate(gap, 0);
     for (let i = 0; i < this.data.length; i++) {
-      stroke(this.chartLineColour);
-      let yLine = -this.data[i][this.yValue] * yStep; //yvalue is what we're displaying on the graph
-      push();
-      fill(this.pointsColour);
+      fill(this.barFill[i % this.barFill.length]);
       noStroke();
-      ellipse(xLine, yLine, this.pointEllipseSize);
-      noFill();
-      pop();
+      rect(0, 0, this.barWidth, -this.data[i][this.yValue] * this.scale);
+      translate(gap + this.barWidth, 0);
 
       // Text X AXIS
       push();
-      noStroke();
-      fill(this.textColour[i % this.textColour.length]);
-      textSize(this.textSize);
+      textSize(this.textSizeText);
       if (this.textRotate === 0) {
         textAlign(CENTER, CENTER);
       } else {
         textAlign(LEFT, CENTER);
       }
-
-      // Draw the Indicator line alongside the text
-      push();
-      translate(xLine, yLine); // Move the origin to the position where the line starts
-      stroke(
-        this.chartLineIndiLineColour[i % this.chartLineIndiLineColour.length]
-      );
-      strokeWeight(1); //breaks indicator lines and they wont show when the property is passed
-      rotate(-20);
-      line(0, 0, 0, 35); //breaks indicator lines and they wont show when the property is passed
-      pop();
-
-      //draw the xValue Labels
-      text(XLabels[i], xLine, yLine + this.xAxisTextYPos);
-      vertex(xLine, yLine);
-      xLine += xStep; //moves the point by taking the first loops value (e.g 2693) and adds the next value to that previous one, these two values are then added together and in the second loop iteration the next value is add.
-
+      rotate(this.textRotate);
+      fill(this.textColour);
+      text(XLabels[i], 0, 30);
+      translate(this.barWidth / 2, 20);
       pop();
     }
-    endShape();
     pop();
 
+    //text xvalue col name
     //subtext
     push();
     noStroke();
@@ -177,10 +152,10 @@ class LineGraphChart {
     text(this.subLabel, this.textSubX, this.textSubY);
     pop();
 
-    //title
+    //text xvalue title name
     push();
     noStroke();
-    fill(this.textTitleColour);
+    fill(this.textColour);
     textFont(this.fontBold);
     textSize(this.textSizeTitle);
     textStyle(this.titleWeight);
@@ -188,6 +163,15 @@ class LineGraphChart {
     text(this.titleText, this.textTitleX, -this.textTitleY, this.titlePaddingX);
     pop();
 
+    //text for col2
+
+    push();
+    noStroke();
+    fill(this.colYAxisColour);
+    textSize(this.colYAxisSize);
+    rotate(this.colYAxisRotation);
+    textStyle(this.colYAxisStyle);
+    text(this.colYAxisTextValue, this.colYAxisTextX, this.colYAXisTextY);
 
     pop();
   }
--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ let font;
 let boldText;
 function preload() {
   data = loadTable("data/FatalCrashes.csv", "csv", "header");
-  font = loadFont("fonts/Inter-Regular.ttf");
-  boldText = loadFont("fonts/Inter-SemiBold.ttf");
+  font = loadFont("Fonts/Inter-Regular.ttf");
+  boldText = loadFont("Fonts/Inter-SemiBold.ttf");
   // italics=loadFont("fonts/");
 }
 
@@ -46,9 +46,9 @@ function setup() {
     textColour: "#0f0000",
     textRotate: 45,
     textSizeText: 13,
-    indiLineRotate:-85,
-    indiLineWeight:1,
-    indiLineHeight:35,
+    indiLineRotate: -85,
+    indiLineWeight: 1,
+    indiLineHeight: 35,
 
     //text for col y axis name
 
@@ -96,69 +96,121 @@ function setup() {
     chartHeight: 200,
     chartWidth: 350,
     xPos: -5,
-    yPos: -350,
+    yPos: -320,
     genFont: font,
     fontBold: boldText,
     pointEllipseSize: 6,
-    chartXYLineWeight:1.5,
+    chartXYLineWeight: 1.5,
 
     //colours
     axisLineColour: "#F5F5F5",
-    ticksColour: "#C72A2A",
-    pointsColour:"#B51C1C",
-    chartLineColour:"#050505",
-    chartLineIndiLineColour:["#8D25A8", "#993BB1", "#B26BC5"],
-
+    ticksValueColour: "#000000",
+    pointsColour: "#B51C1C",
+    chartLineColour: "#050505",
+    chartLineIndiLineColour: ["#8D25A8", "#993BB1", "#B26BC5"],
+    textTitleColour: "#0C0C0C",
+    textColour: ["#8D25A8", "#993BB1", "#B26BC5"],
+    subTextColour: "#020202",
 
     //text for X AXIS
-    textColour: ["#8D25A8", "#993BB1", "#B26BC5"],
     textRotate: 45,
     textSizeText: 13,
-    xAxisTextYPos:45,
+    xAxisTextYPos: 45,
 
-    //text for col y axis name
-
-    colYAxisColour:"#C72A2A",
-    colYAxisSize:16,
-    colYAxisRotation:-90,
-    colYAxisStyle:BOLD,
-    colYAxisTextValue:"no. of deaths",
-    colYAxisTextX:50,
-    colYAxisTextY:-40,
-
-    //text for col
-    textSizeColText: 16,
-    colLabel: "accidents per year",
-    textColY: -420,
-    textColX: 175,
-    textColWeight: BOLD,
-    colVertAlign:CENTER,
-    colHorzAlign:CENTER,
+    //subtext
+    textSizeSub: 12,
+    subLabel: "Accidents Per Year",
+    textSubY: -350,
+    textSubX: 175,
+    subVertAlign: CENTER,
+    subHorzAlign: CENTER,
 
     //text for title
     textSizeTitle: 20,
-    titleText: "Fatal driving accidents resulting from other uneccasary distraction on the road",
-    textTitleX: -25,
-    textTitleY: 600,
-    titlePaddingX: 400,
-    titleWeight: BOLD,
-    titleHorzAlign:CENTER,
-    titleVertAlign:CENTER,
+    titleText:
+      "Fatal traffic incidents caused by avoidable distractions while driving",
+    textTitleX: 15,
+    textTitleY: 590,
+    titlePaddingX: 300,
+    titleHorzAlign: CENTER,
+    titleVertAlign: CENTER,
 
     //tick and tick text
     numTicks: 10,
+    fontBold: boldText,
     ticksTextSize: 13,
     tickStyle: BOLD,
-    tickTextXPos:-10,
-    ticksLength:-5,
+    tickTextXPos: -10,
+    ticksLength: -5,
 
     //values
     yValue: "other-distraction",
     xValue: "Year",
   };
-  barCharts.push(new BarChart(barChart));
+  let horzChart = {
+    data: cleanData,
+    chartHeight: 200,
+    chartWidth: 350,
+    xPos: 560,
+    yPos: 650,
+    barWidth: 25,
+    genFont: font,
+    fontBold: boldText,
 
-  barCharts.push(new LineGraphChart(lineChart));
+    //colours
+    barFill: ["#F7BDD1", "#F59EBB", "#F17CA3"],
+    axisLineColour: "#d9d9d9",
+    ticksColour: "#C72A2A",
+    subTextColour:"C72A2A",
+
+    //text for X AXIS
+    textColour: "#0f0000",
+    textRotate: 45,
+    textSizeText: 13,
+    indiLineRotate: -85,
+    indiLineWeight: 1,
+    indiLineHeight: 35,
+
+    //text for col y axis name
+
+    colYAxisColour: "#C72A2A",
+    colYAxisSize: 16,
+    colYAxisRotation: -90,
+    colYAxisStyle: BOLD,
+    colYAxisTextValue: "no. of deaths",
+    colYAxisTextX: 50,
+    colYAxisTextY: -40,
+
+  //subtext
+  textSizeSub: 12,
+  subLabel: "Accidents Per Year",
+  textSubY: 60,
+  textSubX: 175,
+  subVertAlign: CENTER,
+  subHorzAlign: CENTER,
+
+    //text for title
+    textSizeTitle: 24,
+    titleText:
+      "Fatal driving accidents resulting from the use of mobile devices",
+    textTitleX: -25,
+    textTitleY: 270,
+    titlePaddingX: 400,
+    titleWeight: BOLD,
+    titleHorzAlign: CENTER,
+    titleVertAlign: CENTER,
+
+    //tick and tick text
+    numTicks: 10,
+    ticksTextSize: 13,
+    tickStyle: BOLD,
+
+    //values
+    yValue: "cell-usage",
+    xValue: "Year",
+  };
+  barCharts.push(new BarChart(barChart), new LineGraphChart (lineChart));
+barCharts.push(new HorzBarChart(horzChart));
 }
 
 function draw() {
